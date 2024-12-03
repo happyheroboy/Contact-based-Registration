509460303f28b56654844f91559cf3aaf19d92d9
Xianzhong Xie <73322475+happyheroboy@users.noreply.github.com>
2024-12-03 14:14:54 +0800

    Delete matlab directory

--- matlab/CR/findNearestPts.m
+++
@@ -1,8 +0,0 @@
-function  [minDistance,nearestPoint] = findNearestPts(point,pointList)
-% 计算点到所有点的距离
-distances = sqrt(sum((pointList - point).^2, 2));
-
-% 找到最近的点的索引和距离
-[minDistance, nearestPointIndex] = min(distances);
-nearestPoint = pointList(nearestPointIndex, :);
-end
--- matlab/reset_toolbox.m
+++
@@ -1,6 +0,0 @@
-% clear;
-
-addpath(genpath('./Fuc'));
-addpath(genpath('./Data'));
-addpath(genpath('./Data/Marker-1'));
-addpath(genpath('./CR'));
